287391d8d8c29c4192ad5dee7e7fa61cf4c5cd97
Hatem Atef <hatoom96.ha@gmail.com>
2019-04-30 19:27:59 +0200

    Adding csharp

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,6 @@
 
 import androidelements.WriteXML;
+import csharp.WriteCSharp;
 
 
 public class Runner {
@@ -8,6 +9,7 @@ public class Runner {
             System.out.println("There is no argument to generate");
             System.exit(1);
         }
-        WriteXML xml = new WriteXML(args);
+        //WriteXML xml = new WriteXML(args);
+        WriteCSharp cs = new WriteCSharp(args);
     }
 }
--- src/htmlelements/HTMLElement.java
+++
@@ -1,8 +0,0 @@
-package htmlelements;
-
-import elements.Element;
-import languagewrite.Tag;
-
-public abstract class HTMLElement implements Element {
-    public abstract void draw(Tag root);
-}
